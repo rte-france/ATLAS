509c6eb0139f6005da1bfb32889e316a5f798075
ldegardin <louis.degardin@artelys.com>
2025-03-19 15:04:44 +0100

    copyright

--- atlas_core/math/forecasting_matrix.py
+++ atlas_core/math/forecasting_matrix.py
@@ -1,4 +1,9 @@
 """
+Copyright (c) 2016-2022, RTE (www.rte-france.com)
+See AUTHORS.txt
+SPDX-License-Identifier: MPL-2.0
+This file is part of the ATLAS project.
+
 Module that implements ForecastingMatrix
 """
 
--- atlas_core/scheduling/main.py
+++ atlas_core/scheduling/main.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) 2016-2022, RTE (www.rte-france.com)
+See AUTHORS.txt
+SPDX-License-Identifier: MPL-2.0
+This file is part of the ATLAS project.
+"""
+
 import logging
 from argparse import ArgumentParser
 from datetime import datetime
