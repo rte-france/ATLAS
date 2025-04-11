87060dae13b5721ee0973f96d31ead2195865ae4
lucarammel <lucaspereira0497@gmail.com>
2025-04-11 12:58:55 +0200

    add equal method

--- atlas/math/timeseries.py
+++ atlas/math/timeseries.py
@@ -43,6 +43,16 @@ class Timeseries:
 
             self.timeseries = df
 
+    def __eq__(self, other: pl.DataFrame) -> bool:
+        """Check equality between the internal time series and another Polars DataFrame.
+
+        :param other: The Polars DataFrame to compare with
+        :type other: pl.DataFrame
+        :return: True if the DataFrames are equal, False otherwise
+        :rtype: bool
+        """
+        return self.timeseries.equals(other)
+
     def remove_na(self, inplace: bool = True) -> Timeseries:
         """Remove rows containing null values.
 
--- docs/api/timeseries.md
+++ docs/api/timeseries.md
@@ -1,3 +1,3 @@
-# API Reference
+# Timeseries
 
 ::: atlas.math.timeseries.Timeseries
