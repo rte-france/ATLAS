8a91cca0b647f78a7dcb5c5f2dbbc66e1d4bbde5
boutetale <alexis.boutet@rte-france.com>
2025-04-25 13:00:28 +0200

    Update AbstractParameters class

--- atlas/abstract_class/abstract_parameters.py
+++ atlas/abstract_class/abstract_parameters.py
@@ -3,10 +3,37 @@ Copyright (c) 2025, RTE (www.rte-france.com)
 See AUTHORS.txt
 SPDX-License-Identifier: MPL-2.0
 This file is part of the ATLAS project.
+
+Module that implements AbstractParameters
 """
+from datetime import datetime
 
-from abc import ABC
+from pydantic import BaseModel, model_validator
 
 
-class AbstractDataset(ABC):
-    """Placeholder abstract class for input datasets."""
+class AbstractParameters(BaseModel):
+    """Base class for parameters, to be extended by concrete implementations.
+
+    :param start_date: Study start date
+    :type start_date: datetime
+    :param end_date: Study start date
+    :type end_date: datetime
+    :param execution_date: Study execution date
+    :type execution_date: datetime
+
+    """
+
+    start_date: datetime | None = None
+    end_date: datetime | None = None
+    execution_date: datetime | None = None
+
+    @model_validator(mode='after')
+    def check_dates(self):
+        if self.end_date < self.start_date:
+            raise ValueError(f"Start date '{self.start_date.strftime('%Y-%m-%d %H:%M:%S')}' must be inferior "
+                             f"to end date '{self.end_date.strftime('%Y-%m-%d %H:%M:%S')}'")
+        if not (self.start_date < self.execution_date < self.end_date):
+            raise ValueError(f"Execution date '{self.execution_date.strftime('%Y-%m-%d %H:%M:%S')}' must be between "
+                             f"start date '{self.start_date.strftime('%Y-%m-%d %H:%M:%S')}' and "
+                             f"end date '{self.end_date.strftime('%Y-%m-%d %H:%M:%S')}'")
+        return self
