856e9ddfe6c4acae5e33330cb54ade59859e3193
boutetale <alexis.boutet@rte-france.com>
2025-05-09 16:07:48 +0200

    [market clearing] First optim variable in clearing phase

--- atlas/modules/market_clearing/market_clearing_input_dataset.py
+++ atlas/modules/market_clearing/market_clearing_input_dataset.py
@@ -4,13 +4,26 @@ SPDX-License-Identifier: MPL-2.0
 This file is part of the ATLAS project.
 """
 
-from atlas import BusinessModel
+from atlas.models.business_model import BusinessModel
 from atlas.abstract_class.abstract_dataset import AbstractDataset
 from atlas.modules.market_clearing.market_clearing_parameters import MarketClearingParameters
 
 
 class MarketClearingInputDataset(AbstractDataset[MarketClearingParameters]):
     """Input dataset for Market Clearing module"""
+
+    def __init__(self, parameters: MarketClearingParameters):
+        self.parameters = parameters
+        self.market_areas = None
+        self.times = []
+
+    def set_market_areas(self, market_areas) -> None:
+        if self.parameters.market == "All":
+            self.market_areas = market_areas
+        else:
+            self.market_areas = [
+                market_area for market_area in market_areas if market_area.name in self.parameters.market
+            ]
 
     def get_business_model_class_used(self) -> list[type[BusinessModel]]:
         return []
