b774e355e7568e0b371a8f9d7bd12f62dc2bb1be
ldegardin <louis.degardin@artelys.com>
2025-05-14 11:22:49 +0200

    added workflow test

--- tests/test_unit/test_workflow/test_workflow.py
+++ tests/test_unit/test_workflow/test_workflow.py
@@ -5,15 +5,19 @@ from atlas import BusinessModel
 from atlas.abstract_class.abstract_dataset import input_dataset_type_var, output_dataset_type_var, AbstractDataset
 from atlas.abstract_class.abstract_module import AbstractModule
 from atlas.abstract_class.abstract_parameters import module_parameters_type_var, AbstractParameters
+from atlas.workflow.workflow import Workflow
 from atlas.workflow.workflow_helper import WorkflowHelper
 from atlas.workflow.workflow_parameters import WorkflowParameters
+from atlas.workflow.workflow_step import WorkflowStep
 
 
 class ModuleTest(AbstractModule):
     def execute(
         self, parameters: module_parameters_type_var, input_dataset: input_dataset_type_var
     ) -> output_dataset_type_var:
-        return DatasetTest()
+        output_dataset = input_dataset
+        output_dataset.data += 1
+        return output_dataset
 
     def create_parameters(self, raw_params: dict[str, Any]) -> module_parameters_type_var:
         """Creates a concrete parameters object from raw dictionary."""
@@ -53,6 +57,9 @@ class ModuleParamsTest(AbstractParameters):
 
 
 class DatasetTest(AbstractDataset):
+    def __init__(self, filepath):
+        self.data = 0
+
     def get_business_model_class_used(self) -> list[type[BusinessModel]]:
         return []
 
@@ -66,8 +73,22 @@ def test_parameters_reading():
 
 def test_basic_workflow():
     wfp = WorkflowParameters(Path("workflow_parameters.yml"))
-    datasetTest = DatasetTest()
+    datasetTest = DatasetTest(None)
     module_params = ModuleParamsTest(wfp.steps["step1"]["parameters"])
     wf = WorkflowHelper.create_simple_workflow(datasetTest, module_params, ModuleTest())
     wf.execute()
     assert wf.get_output_dataset() is not None
+
+
+def test_workflow():
+    wfp = WorkflowParameters(Path("workflow_parameters.yml"))
+    datasetTest = DatasetTest(None)
+    params_step1 = ModuleParamsTest(wfp.steps["step1"]["parameters"])
+    params_step2 = ModuleParamsTest(wfp.steps["step2"]["parameters"])
+
+    wf = Workflow("wf_test", datasetTest, None)
+    wf.add_step(WorkflowStep("step1", params_step1, ModuleTest()))
+    wf.add_step(WorkflowStep("step2", params_step2, ModuleTest()))
+    assert len(wf.steps) == 2
+    wf.execute()
+    assert wf.get_output_dataset().data == 2
