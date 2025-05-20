f9224504e9af3f147c3797d4bc8b3e925ab2cf61
lucarammel <lucaspereira0497@gmail.com>
2025-05-20 13:50:47 +0200

    enable empty scenariomatrix

--- atlas/math/scenario_matrix.py
+++ atlas/math/scenario_matrix.py
@@ -23,7 +23,7 @@ class ScenarioMatrix(Matrix):
     This class is intended for workflows where all data can be loaded and processed
     in memory."""
 
-    def __init__(self, matrix: pd.DataFrame | pl.DataFrame, timezone: str = "UTC") -> None:
+    def __init__(self, matrix: pd.DataFrame | pl.DataFrame | Matrix | None = None, timezone: str = "UTC") -> None:
         super().__init__(matrix, timezone)
 
     def __repr__(self):
