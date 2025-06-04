739c42c325ef1727ece1e8d067ae33826eea0e97
ldegardin <louis.degardin@artelys.com>
2025-06-04 15:50:56 +0200

    translate first step of day ahead orders

--- atlas/modules/day_ahead_orders/day_ahead_orders_module.py
+++ atlas/modules/day_ahead_orders/day_ahead_orders_module.py
@@ -7,13 +7,14 @@ This file is part of the ATLAS project.
 
 from typing import Any
 
-from atlas import BusinessModel
-from atlas.abstract_class.abstract_dataset import input_dataset_type_var, output_dataset_type_var
+from atlas import BusinessModel, Logger
 from atlas.abstract_class.abstract_module import AbstractModule
 from atlas.abstract_class.abstract_parameters import module_parameters_type_var
 from atlas.modules.day_ahead_orders.day_ahead_orders_input_dataset import DayAheadOrdersInputDataset
 from atlas.modules.day_ahead_orders.day_ahead_orders_output_dataset import DayAheadOrdersOutputDataset
 from atlas.modules.day_ahead_orders.day_ahead_orders_parameters import DayAheadOrdersParameters
+from atlas.modules.day_ahead_orders.orders_formulation.load import Load
+from atlas.modules.day_ahead_orders.tools.Utilities import Utilities
 
 
 class DayAheadOrdersModule(
@@ -25,35 +26,87 @@ class DayAheadOrdersModule(
 
     def import_data(
         self, raw_data: dict[str, list[BusinessModel]], parameters: module_parameters_type_var
-    ) -> input_dataset_type_var:
+    ) -> DayAheadOrdersInputDataset:
         """Imports data using business objects and parameters."""
         return DayAheadOrdersInputDataset(raw_data, parameters)
 
-    def validate_data(self, parameters: module_parameters_type_var, input_dataset: input_dataset_type_var) -> bool:
+    def validate_data(self, parameters: module_parameters_type_var, input_dataset: DayAheadOrdersInputDataset) -> bool:
         """Validates imported or generated data."""
         pass
 
     def validates_results(
         self,
-        parameters: module_parameters_type_var,
-        input_dataset: input_dataset_type_var,
-        output_dataset: output_dataset_type_var,
+        parameters: DayAheadOrdersParameters,
+        input_dataset: DayAheadOrdersInputDataset,
+        output_dataset: DayAheadOrdersOutputDataset,
     ) -> bool:
         """Validates results"""
         pass
 
     def export_results(
         self,
-        parameters: module_parameters_type_var,
-        input_dataset: input_dataset_type_var,
-        output_dataset: output_dataset_type_var,
+        parameters: DayAheadOrdersParameters,
+        input_dataset: DayAheadOrdersInputDataset,
+        output_dataset: DayAheadOrdersOutputDataset,
     ) -> None:
         """Exports results."""
         pass
 
     def execute(
-        self, parameters: module_parameters_type_var, input_dataset: input_dataset_type_var
-    ) -> output_dataset_type_var:
+        self, parameters: DayAheadOrdersParameters, input_dataset: DayAheadOrdersInputDataset
+    ) -> DayAheadOrdersOutputDataset:
         """Executes the module's main logic."""
 
-    pass
+        logger = Logger()
+
+        # Formulation of bids and orders on the day-ahead market.
+        # In practice, several functions are run. Each function extract data from the input marker
+        # and formulates bids or offers in the output marker. The latter is of class "Offer".
+
+        #### STEP 0 - INITIALIZATION ####
+        logger.get_logger().info("Initialization of the Day-Ahead Orders module...")
+
+        # output_dataset = DayAheadOrdersOutputDataset()
+        output_dataset = input_dataset
+
+        # Create the sequence of orders times. In particular, this sequence is such that the endDate of the last order will be before
+        # the endDate of the overall time frame.
+        orders_time = Utilities.define_orders_time(parameters)
+        logger.get_logger().info("Extraction completed, now starting the formulation of orders...")
+
+        #### STEP 1 - CONSUMPTION ####
+        logger.get_logger().info("Formulation of the load orders...")
+        Load.formulate_load_orders(output_dataset, orders_time, parameters)
+        logger.get_logger().info("Consumption orders formulated.")
+
+        """
+        #### STEP 2 - NON DISPATCHABLE UNITS ####
+        API.IO.Trace.Log("Formulation of the non-dispatchable orders...", API.IO.LogTypeInfo)
+        non_dispatchable.formulate_non_dispatchable_orders(input_dataset, output_dataset, orders_time, parameters)
+        API.IO.Trace.Log("Non-dispatchable orders formulated.", API.IO.LogTypeInfo)
+
+        #### STEP 3 - STORAGE UNITS ####
+        API.IO.Trace.Log("Formulation of the storage orders...", API.IO.LogTypeInfo)
+        storage.formulate_storage_orders(input_dataset, output_dataset, parameters)
+        API.IO.Trace.Log("Storage orders formulated.", API.IO.LogTypeInfo)
+
+        #### STEP 4 - LAKES UNITS ####
+        API.IO.Trace.Log("Formulation of the hydraulic orders...", API.IO.LogTypeInfo)
+        hydraulic.formulate_hydraulic_orders(input_dataset, output_dataset, orders_time, parameters)
+        API.IO.Trace.Log("Hydraulic orders formulated.", API.IO.LogTypeInfo)
+
+        #### STEP 5 - WIND AND PV UNITS ####
+        API.IO.Trace.Log("Formulation of the wind/pv orders...", API.IO.LogTypeInfo)
+        wind_pv.formulate_wind_and_pv_orders(input_dataset, output_dataset, orders_time, parameters)
+        API.IO.Trace.Log("Non-dispatchable orders formulated.", API.IO.LogTypeInfo)
+
+        #### STEP 6 - THERMIC UNITS ####
+        API.IO.Trace.Log("Formulation of the thermic orders...", API.IO.LogTypeInfo)
+        thermic_bidding.formulate_thermic_orders(input_dataset, output_dataset, orders_time, parameters)
+        API.IO.Trace.Log("Thermic orders formulated.", API.IO.LogTypeInfo)
+
+        #### STEP - INDICATE TO THE USER THAT THE FORMULATION OF ORDERS IS COMPLETED.
+        API.IO.Trace.Log("Formulation of orders successfully completed.", API.IO.LogTypeInfo)
+        """
+
+        # return output_dataset
---
+++ atlas/modules/day_ahead_orders/tools/Utilities.py
@@ -0,0 +1,113 @@
+"""
+Copyright (c) 2025, RTE (www.rte-france.com)
+
+SPDX-License-Identifier: MPL-2.0
+This file is part of the ATLAS project.
+"""
+
+import datetime
+import os
+import sys
+
+from atlas import Logger
+
+
+## NB. sys will not be used in the future. Instead, use a function that interrupts the system.
+## once this is implemented, remove the
+
+
+# Contains miscellaneous functions used in the various files.
+class Utilities:
+    def datetime_parser(DateString):
+        """
+        Converts a string into a variable in DateTime format.
+        Returns a detailed error if the conversion fails.
+        Arguments:
+        - `DateString`: a date (dd/mm/yyy - hh:mm:ss) in string format
+        """
+
+        try:
+            result_date = API.DatetimeIndex.ParseDate(DateString)
+        except SystemError:
+            msg = "One of the dates entered cannot be converted. Please check that the format is in correct form (e.g. 'dd/mm/yyy hh:mm:ss')"
+            API.IO.Trace.Log(msg, API.IO.LogTypeError)
+            sys.exit()  ### FUTURE WARNING : WILL HAVE TO BE REPLACED BY AN API FUNCTION THAT STOPS THE EXECUTION OF THE PROGRAM ASAP
+        return result_date
+
+    def get_date_to_clean_string(date):
+        """
+        Converts a datetime object to a string without special characters
+        """
+        string = str(date)
+        string = string.replace("/", "_").replace(":", "_").replace(" ", "_")
+        return string
+
+    def define_orders_time(p) -> list[datetime]:
+        """
+        This function creates a sequence of timestamps between a startDate and a endDate
+        with step deltaTime. It returns a list of dateTime objects.
+        In particular, it makes sure that no time step crosses the endDate boundary.
+
+        Arguments:
+        - `p` a named tuple of subclass Parameters_List containing the dates.
+        """
+
+        if p.start_date < p.end_date:
+            orders_time = Utilities.new_index(p.start_date, p.end_date.AddMinutes(-p.time_step), str(p.time_step) + "m")
+        else:
+            msg = "The EndDate parameter must be posterior to the StartDate parameter."
+            Logger.get_logger().error(msg)
+            # sys.exit()
+        return orders_time
+
+    @staticmethod
+    def new_index(start_date: datetime, end_date: datetime, interval: datetime.timedelta) -> list[datetime]:
+        """
+        Generate a list of datetime between `start` and `end` depending on the given interval.
+
+        :param start_date: first datetime
+        :param end_date: last datetime
+        :param interval: time interval (timedelta)
+        :return: datetime list
+        """
+        if start_date >= end_date:
+            raise ValueError("start date must be before end date.")
+
+        result = []
+        current = start_date
+        while current <= end_date:
+            result.append(current)
+            current = current + interval
+
+        return result
+
+    def QuickerGetForecast(matrix, execution_date, start_date, end_date):
+        """
+        This function is created to limit calls to the GetForecast method which is time-expensive.
+        If conditions are met, the far quicker GetTimeSeries method is called
+
+        Arguments:
+        - `matrix` a Forecasting Matrix
+        - `execution_date` the desired reference date within
+        - `start_date` the start date to crop the ts with (to limit its size), if GetForecast is called
+        - `end_date` the end date to crop the ts with (to limit its size), if GetForecast is called
+        """
+        if execution_date in matrix.Index:
+            # An Extract between start_date and end_date might be necessary here, but not usefull as of ATLAS 1.2
+            return matrix.GetTimeSeries(execution_date)
+
+        return matrix.GetForecast(execution_date, start_date, end_date)
+
+    # Helper used to manage the output path of debug data
+    def check_output_path(output_path):
+        """
+        Takes as input a path (in our case, in the SAMBA output folder of the user),
+        and checks if this path exists.
+        If not, a folder is created and the user is notified with a message in the console
+        """
+
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+            API.IO.Trace.Log("Output folder for debug created at {}".format(output_path), API.IO.LogTypeInfo)
+
+        return None
