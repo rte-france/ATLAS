30a8f83dfd5de0e88c5ea0e8f7ff9277da169532
aboutet <alexis.boutet@artelys.com>
2025-07-01 14:36:45 +0200

    Create exchange fixing model

--- atlas/modules/market_clearing/marker_clearing_module.py
+++ atlas/modules/market_clearing/marker_clearing_module.py
@@ -12,6 +12,7 @@ from atlas.modules.market_clearing.market_clearing_input_dataset import MarketCl
 from atlas.modules.market_clearing.market_clearing_output_dataset import MarketClearingOutputDataset
 from atlas.modules.market_clearing.market_clearing_parameters import MarketClearingParameters
 from atlas.modules.market_clearing.phases.clearing import Clearing
+from atlas.modules.market_clearing.phases.exchanges_fixing import ExchangesFixing
 from atlas.modules.market_clearing.phases.marginal_fixing import MarginalFixing
 
 
@@ -42,6 +43,8 @@ class MarketClearingModule(
         clearing = Clearing(input_dataset, parameters)
         clearing.run()
         # Launch Exchange Fixing phase
+        exchange_fixing = ExchangesFixing(input_dataset, parameters)
+        exchange_fixing.run(clearing)
         # Launch Pricing phase
         market_prices = {}  # retrieve from pricing
         marginal_fixing = MarginalFixing(input_dataset, parameters)
