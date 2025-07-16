8fbb2899351934b9e6653d63027795386a18a423
ldegardin <louis.degardin@artelys.com>
2025-07-16 18:02:01 +0200

    add tests for solver_helper.py

--- atlas/solver/solver_helper.py
+++ atlas/solver/solver_helper.py
@@ -3,8 +3,6 @@ Copyright (c) 2025, RTE (www.rte-france.com)
 
 SPDX-License-Identifier: MPL-2.0
 This file is part of the ATLAS project.
-
-Module that implements OR-Tools optimisation interface.
 """
 
 from collections import OrderedDict
