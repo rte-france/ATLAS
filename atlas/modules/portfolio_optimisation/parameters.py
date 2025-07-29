5df4018a43433626cbca1f3c353e8aca3c5175c1
lucarammel <lucaspereira0497@gmail.com>
2025-07-29 13:54:44 +0200

    update duration params

--- atlas/modules/portfolio_optimisation/parameters.py
+++ atlas/modules/portfolio_optimisation/parameters.py
@@ -75,30 +75,39 @@ class PortfolioOptimisationParameters(AbstractParameters):
         description="Quantity (%) of imbalance qualified as small, relative to max portfolio energy.",
     )
     solver_duality_gap: float = Field(0.0001, description="Duality gap used for the optimization.")
-    battery_automated_reserve_duration: int = Field(60, description="Automated reserve duration for battery equipment.")
+    battery_automated_reserve_duration: Duration = Field(
+        default_factory=lambda: duration(minutes=60), description="Automated reserve duration for battery equipment."
+    )
     battery_number_of_fragments: int = Field(
         3, description="Number of power fragments for battery; last fragments are more expensive."
     )
-    battery_reserve_duration: int = Field(60, description="Manual reserve duration for battery equipment.")
-    electric_vehicle_automated_reserve_duration: int = Field(
-        1, description="Automated reserve duration for electric vehicle equipment."
+    battery_reserve_duration: Duration = Field(
+        default_factory=lambda: duration(minutes=60), description="Manual reserve duration for battery equipment."
+    )
+    electric_vehicle_automated_reserve_duration: Duration = Field(
+        default_factory=lambda: duration(minutes=1),
+        description="Automated reserve duration for electric vehicle equipment.",
     )
     electric_vehicle_number_of_fragments: int = Field(3, description="Number of power fragments for electric vehicle.")
-    electric_vehicle_reserve_duration: int = Field(
-        0, description="Manual reserve duration for electric vehicle equipment."
+    electric_vehicle_reserve_duration: Duration = Field(
+        lambda: duration(minutes=1), description="Manual reserve duration for electric vehicle equipment."
     )
     hydraulic_minimal_fragment_size: int = Field(
         100, description="Minimal amount of power for an offer to be formulated for hydraulic."
     )
-    pumped_hydraulic_automated_reserve_duration: int = Field(
-        60, description="Automated reserve duration for pumped hydraulic equipment."
+    pumped_hydraulic_automated_reserve_duration: Duration = Field(
+        default_factory=lambda: duration(minutes=60),
+        description="Automated reserve duration for pumped hydraulic equipment.",
     )
     pumped_hydraulic_number_of_fragments: int = Field(3, description="Number of power fragments for pumped hydraulic.")
-    pumped_hydraulic_reserve_duration: int = Field(
-        60, description="Manual reserve duration for pumped hydraulic equipment."
+    pumped_hydraulic_reserve_duration: Duration = Field(
+        default_factory=lambda: duration(minutes=60),
+        description="Manual reserve duration for pumped hydraulic equipment.",
     )
 
-    solver_timeout: int = Field(240, description="Timeout (in seconds) of the optimization.")
+    solver_timeout: Duration = Field(
+        default_factory=lambda: duration(seconds=60), description="Timeout (in seconds) of the optimization."
+    )
     excluded_market_areas_: str | None = Field(
         None,
         description='list of market areas (separated by ";") excluded from classic optimization. None and "all" are possible values.',
@@ -124,31 +133,31 @@ class PortfolioOptimisationParameters(AbstractParameters):
     )
 
     additional_hours: Duration = Field(
-        default_factory=lambda: duration(hours=12),  # 12 hours
+        default_factory=lambda: duration(hours=12),
         description="Default optimization period in hours for PV, Wind, and Load. Overwritten by specific equipment.",
     )
     battery_additional_hours: Duration = Field(
-        default_factory=lambda: duration(hours=48),  # 48 hours
+        default_factory=lambda: duration(hours=48),
         description="Optimization period in hours for Storage Equipments of type Battery.",
     )
     electric_vehicle_additional_hours: Duration = Field(
-        default_factory=lambda: duration(hours=0),  # 0 hours
+        default_factory=lambda: duration(hours=0),
         description="Optimization period in hours for Storage Equipments of type ElectricVehicle.",
     )
     hydraulic_additional_hours: Duration = Field(
-        default_factory=lambda: duration(hours=12),  # 12 hours
+        default_factory=lambda: duration(hours=12),
         description="Optimization period in hours for hydraulic group.",
     )
     pumped_hydraulic_storage_additional_hours: Duration = Field(
-        default_factory=lambda: duration(hours=144),  # 144 hours
+        default_factory=lambda: duration(hours=144),
         description="Optimization period in hours for Storage Equipments of type PumpedHydraulicStorage.",
     )
     thermal_additional_hours: Duration = Field(
-        default_factory=lambda: duration(hours=12),  # 12 hours
+        default_factory=lambda: duration(hours=12),
         description="Optimization period in hours for thermal group.",
     )
     timestep: Duration = Field(
-        default_factory=lambda: duration(hours=1),  # 1 hour
+        default_factory=lambda: duration(hours=1),
         description="Time step (in hours) of the simulated market.",
     )
 
@@ -171,6 +180,9 @@ class PortfolioOptimisationParameters(AbstractParameters):
         "pumped_hydraulic_automated_reserve_duration",
         "battery_automated_reserve_duration",
         "electric_vehicle_automated_reserve_duration",
+        "electric_vehicle_reserve_duration",
+        "battery_reserve_duration",
+        "pumped_hydraulic_reserve_duration",
         mode="before",
     )
     @classmethod
@@ -231,7 +243,7 @@ class PortfolioOptimisationParameters(AbstractParameters):
 
     @property
     def thermal_optimization_period(self) -> int:
-        return len(self.target_times) + int(self.thermal_additional_hours * 60.0 / self.timestep)
+        return len(self.target_times) + int(self.thermal_additional_hours / self.timestep)
 
     @property
     def thermal_op_times(self) -> list[DateTime]:
