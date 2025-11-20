f611410a3fea7eaa2924ff26876b7eba9f10fbd3
ldegardin <louis.degardin@artelys.com>
2025-11-20 17:57:03 +0100

    initialize empty lists in input dataset

--- atlas/modules/day_ahead_orders/dao_input_dataset.py
+++ atlas/modules/day_ahead_orders/dao_input_dataset.py
@@ -32,20 +32,46 @@ class DayAheadOrdersInputDataset(AbstractDataset[DayAheadOrdersParameters]):
         self.raw_data = raw_data
         self.parameters = parameters
 
-        self.control_block = raw_data[INVERSE_MODEL_MAPPING_NAME[ControlBlock]]
-        self.market_area = raw_data[INVERSE_MODEL_MAPPING_NAME[MarketArea]]
-        self.market_border = raw_data[INVERSE_MODEL_MAPPING_NAME[MarketBorder]]
-        self.node = raw_data[INVERSE_MODEL_MAPPING_NAME[Node]]
-        self.portfolio = raw_data[INVERSE_MODEL_MAPPING_NAME[Portfolio]]
-        self.wind = raw_data[INVERSE_MODEL_MAPPING_NAME[Wind]]
-        self.storage = raw_data[INVERSE_MODEL_MAPPING_NAME[Storage]]
-        self.hydro = raw_data[INVERSE_MODEL_MAPPING_NAME[Hydro]]
-        self.solar = raw_data[INVERSE_MODEL_MAPPING_NAME[Solar]]
-        self.thermal = raw_data[INVERSE_MODEL_MAPPING_NAME[Thermal]]
-        self.other_non_dispatchable = raw_data[INVERSE_MODEL_MAPPING_NAME[OtherNonDispatchable]]
+        self.control_block = (
+            raw_data[INVERSE_MODEL_MAPPING_NAME[ControlBlock]]
+            if INVERSE_MODEL_MAPPING_NAME[ControlBlock] in raw_data
+            else []
+        )
+        self.market_area = (
+            raw_data[INVERSE_MODEL_MAPPING_NAME[MarketArea]]
+            if INVERSE_MODEL_MAPPING_NAME[MarketArea] in raw_data
+            else []
+        )
+        self.market_border = (
+            raw_data[INVERSE_MODEL_MAPPING_NAME[MarketBorder]]
+            if INVERSE_MODEL_MAPPING_NAME[MarketBorder] in raw_data
+            else []
+        )
+        self.node = raw_data[INVERSE_MODEL_MAPPING_NAME[Node]] if INVERSE_MODEL_MAPPING_NAME[Node] in raw_data else []
+        self.portfolio = (
+            raw_data[INVERSE_MODEL_MAPPING_NAME[Portfolio]] if INVERSE_MODEL_MAPPING_NAME[Portfolio] in raw_data else []
+        )
+        self.wind = raw_data[INVERSE_MODEL_MAPPING_NAME[Wind]] if INVERSE_MODEL_MAPPING_NAME[Wind] in raw_data else []
+        self.storage = (
+            raw_data[INVERSE_MODEL_MAPPING_NAME[Storage]] if INVERSE_MODEL_MAPPING_NAME[Wind] in raw_data else []
+        )
+        self.hydro = (
+            raw_data[INVERSE_MODEL_MAPPING_NAME[Hydro]] if INVERSE_MODEL_MAPPING_NAME[Hydro] in raw_data else []
+        )
+        self.solar = (
+            raw_data[INVERSE_MODEL_MAPPING_NAME[Solar]] if INVERSE_MODEL_MAPPING_NAME[Solar] in raw_data else []
+        )
+        self.thermal = (
+            raw_data[INVERSE_MODEL_MAPPING_NAME[Thermal]] if INVERSE_MODEL_MAPPING_NAME[Thermal] in raw_data else []
+        )
+        self.other_non_dispatchable = (
+            raw_data[INVERSE_MODEL_MAPPING_NAME[OtherNonDispatchable]]
+            if INVERSE_MODEL_MAPPING_NAME[OtherNonDispatchable] in raw_data
+            else []
+        )
         self.order: list[Order] = []
         self.order_coupling: list[OrderCoupling] = []
-        self.load = raw_data[INVERSE_MODEL_MAPPING_NAME[Load]]
+        self.load = raw_data[INVERSE_MODEL_MAPPING_NAME[Load]] if INVERSE_MODEL_MAPPING_NAME[Load] in raw_data else []
 
     def get_business_model_class_used(self) -> list[type[BusinessModel]]:
         return []
