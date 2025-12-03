25f5d7f4e4def2eedd72b3797ae029521c9cff21
lucarammel <lucaspereira0497@gmail.com>
2025-12-03 11:28:20 +0100

    reorder import in test

--- tests/test_unit/test_math/test_lazy_matrix.py
+++ tests/test_unit/test_math/test_lazy_matrix.py
@@ -4,6 +4,7 @@ import polars as pl
 import pytest
 
 from atlas.math.lazy_matrix import LazyMatrix
+from atlas.math.lazy_timeseries import LazyTimeseries
 from atlas.math.matrix import Matrix
 from atlas.math.scenario_matrix import LazyScenarioMatrix, ScenarioMatrix
 
@@ -162,7 +163,6 @@ def test_preserves_lazy_evaluation(simple_lazyframe):
 
 def test_select(simple_lazyframe):
     """Test select() returns a LazyTimeseries."""
-    from atlas.math.lazy_timeseries import LazyTimeseries
 
     matrix = LazyMatrix(simple_lazyframe)
     ts = matrix.select("1")
