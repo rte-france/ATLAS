23a358d4b92efffa4465cdb74e89cbe5466ce1a3
aboutet <alexis.boutet@artelys.com>
2025-12-03 16:54:39 +0100

    Fix errors on output dataset

--- atlas/modules/market_clearing/market_clearing_output_dataset.py
+++ atlas/modules/market_clearing/market_clearing_output_dataset.py
@@ -113,9 +113,9 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
 
     def update_raw_data_with_not_modified_business_model_object(self):
         """ Update raw_data with business model object that have not changed"""
-        for business_model in cfg.INVERSE_MODEL_MAPPING_NAME:
-            if business_model not in self.get_custom_business_model_object_modified():
-                business_model_name = cfg.INVERSE_MODEL_MAPPING_NAME[business_model]
+        for business_model_name in self.input_dataset.raw_data:
+            if (cfg.MODEL_MAPPING_NAME[business_model_name] not in
+                    MarketClearingOutputDataset.get_custom_business_model_object_modified()):
                 self.raw_data[business_model_name] = self.input_dataset.raw_data[business_model_name]
 
         # Create not modified MarketArea
@@ -132,10 +132,12 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
                 self.raw_data[market_border_business_object_str].append(market_border)
         # Create not modified CriticalBranch
         critical_branch_business_object_str = cfg.INVERSE_MODEL_MAPPING_NAME[CriticalBranch]
-        self.raw_data[critical_branch_business_object_str] = []
-        for critical_branch in self.input_dataset.raw_data[critical_branch_business_object_str]:
-            if critical_branch.name not in self.input_dataset.mc_critical_branches:
-                self.raw_data[critical_branch_business_object_str].append(critical_branch)
+        # If there is no critical branch (we may be in ATC
+        if critical_branch_business_object_str in self.input_dataset.raw_data:
+            self.raw_data[critical_branch_business_object_str] = []
+            for critical_branch in self.input_dataset.raw_data[critical_branch_business_object_str]:
+                if critical_branch.name not in self.input_dataset.mc_critical_branches:
+                    self.raw_data[critical_branch_business_object_str].append(critical_branch)
         # Create not modified Order
         order_business_object_str = cfg.INVERSE_MODEL_MAPPING_NAME[Order]
         self.raw_data[order_business_object_str] = []
@@ -147,24 +149,24 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
         """ Update raw_data with business model object that have changed
         [MarketArea, MarketBorder, CriticalBranch, Order, Equipment, Portfolio]"""
         # Create modified MarketArea
-        for mc_market_area in self.input_dataset.mc_market_areas.items():
+        for mc_market_area in self.input_dataset.mc_market_areas.values():
             mc_market_area_dump = MarketClearingInputDataset.shallow_dump(mc_market_area)
             market_area = MarketArea.model_validate(mc_market_area_dump)
             self.raw_data[cfg.INVERSE_MODEL_MAPPING_NAME[MarketArea]].append(market_area)
         # Create modified MarketBorder
-        for mc_market_border in self.input_dataset.mc_market_borders.items():
+        for mc_market_border in self.input_dataset.mc_market_borders.values():
             mc_market_border_dump = MarketClearingInputDataset.shallow_dump(mc_market_border)
-            market_border = MarketArea.model_validate(mc_market_border_dump)
+            market_border = MarketBorder.model_validate(mc_market_border_dump)
             self.raw_data[cfg.INVERSE_MODEL_MAPPING_NAME[MarketArea]].append(market_border)
         # Create modified CriticalBranch
-        for mc_critical_branch in self.input_dataset.mc_critical_branches.items():
+        for mc_critical_branch in self.input_dataset.mc_critical_branches.values():
             mc_critical_branch_dump = MarketClearingInputDataset.shallow_dump(mc_critical_branch)
-            critical_branch = MarketArea.model_validate(mc_critical_branch_dump)
+            critical_branch = CriticalBranch.model_validate(mc_critical_branch_dump)
             self.raw_data[cfg.INVERSE_MODEL_MAPPING_NAME[MarketArea]].append(critical_branch)
         # Create modified Order
-        for mc_order in self.input_dataset.mc_orders.items():
+        for mc_order in self.input_dataset.mc_orders.values():
             mc_order_dump = MarketClearingInputDataset.shallow_dump(mc_order)
-            order = MarketArea.model_validate(mc_order_dump)
+            order = Order.model_validate(mc_order_dump)
             self.raw_data[cfg.INVERSE_MODEL_MAPPING_NAME[MarketArea]].append(order)
 
 
@@ -183,9 +185,9 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
 
     def update_orders(self, accepted_powers: dict[tuple[str, str], float], market_prices: dict[tuple[str, int], float]):
         null_timeseries = Timeseries.from_index(
-            self.input_dataset.parameters.times[0],
+            self.input_dataset.times[0],
             self.input_dataset.parameters.time_step,
-            self.input_dataset.parameters.times[-1],
+            self.input_dataset.times[-1],
             0.0
         )
         # Updating orders
@@ -264,9 +266,9 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
                         portfolio.fcr_activated = self.update_timeseries(
                             portfolio.fcr_activated, value_sold_ts)
                     case Product.Intraday:
-                        equipment.total_id_cleared_quantity = self.update_timeseries(
+                        equipment.total_id_cleared_quantity = self.add_timeseries(
                             equipment.total_id_cleared_quantity, value_sold_ts)
-                        portfolio.total_id_cleared_quantity = self.update_timeseries(
+                        portfolio.total_id_cleared_quantity = self.add_timeseries(
                             portfolio.total_id_cleared_quantity, value_sold_ts)
                         equipment.id_cleared_quantity = self.update_forecast_timeseries(
                             equipment.id_cleared_quantity, value_sold_ts)
@@ -284,8 +286,8 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
     def update_market_area(self, local_balances: dict[tuple[str, int], float],
                            market_prices: dict[tuple[str, int], float]):
         for market_area_name, mc_market_area in self.input_dataset.mc_market_areas.items():
-            balance_values = [local_balances[market_area_name, time_index] for time_index in len(self.input_dataset.times)]
-            price_values = [market_prices[market_area_name, time_index] for time_index in len(self.input_dataset.times)]
+            balance_values = [local_balances[market_area_name, time_index] for time_index, _ in enumerate(self.input_dataset.times)]
+            price_values = [market_prices[market_area_name, time_index] for time_index, _ in enumerate(self.input_dataset.times)]
 
             values_bal = Timeseries.from_values(
                 self.input_dataset.parameters.start_date,
@@ -301,10 +303,9 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
             match self.input_dataset.parameters.market:
                 case Product.DayAhead:
                     mc_market_area.da_balance = self.update_timeseries(mc_market_area.da_balance, values_bal)
-                    mc_market_area.da_balance = self.update_timeseries(mc_market_area.da_balance, values_bal)
-                    mc_market_area.da_price += values_price
+                    mc_market_area.da_price = self.update_timeseries(mc_market_area.da_price, values_bal)
                 case Product.Intraday:
-                    mc_market_area.total_id_balance = self.update_timeseries(mc_market_area.total_id_balance, values_bal)
+                    mc_market_area.total_id_balance = self.add_timeseries(mc_market_area.total_id_balance, values_bal)
                     mc_market_area.id_balance = self.update_forecast_timeseries(
                         mc_market_area.id_balance, values_bal)
                     mc_market_area.id_price = self.update_forecast_timeseries(
@@ -325,11 +326,11 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
         for market_border_name, mc_market_border in self.input_dataset.mc_market_borders.items():
 
             flow_values = [border_exchanges[market_border_name, time_index]
-                           for time_index in len(self.input_dataset.times)]
+                           for time_index, _ in enumerate(self.input_dataset.times)]
             shadow_price_values = [
-                market_prices[mc_market_border.uphill_market_area, time_index] -
-                market_prices[mc_market_border.downhill_market_area, time_index]
-                for time_index in len(self.input_dataset.times)]
+                market_prices[mc_market_border.uphill_market_area.name, time_index] -
+                market_prices[mc_market_border.downhill_market_area.name, time_index]
+                for time_index, _ in enumerate(self.input_dataset.times)]
 
             flow = Timeseries.from_values(
                 self.input_dataset.parameters.start_date,
@@ -348,7 +349,7 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
                     mc_market_border.da_shadow_price = self.update_timeseries(
                                 mc_market_border.da_shadow_price, shadow_price)
                 case Product.Intraday:
-                    mc_market_border.total_id_flow = self.update_timeseries(
+                    mc_market_border.total_id_flow = self.add_timeseries(
                         mc_market_border.total_id_flow, flow)
                     mc_market_border.id_flow = self.update_forecast_timeseries(
                         mc_market_border.id_flow, flow)
@@ -365,7 +366,7 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
                         mc_market_border.afrr_up_procured, flow)
                 case Product.AFRRDownProcurement:
                     mc_market_border.afrr_down_procured = self.update_timeseries(
-                        mc_market_border.total_id_flow, flow)
+                        mc_market_border.afrr_down_procured, flow)
                 case Product.RRUpProcurement:
                     mc_market_border.rr_up_procured = self.update_timeseries(
                         mc_market_border.rr_up_procured, flow)
@@ -401,7 +402,7 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
         for critical_branch_name, mc_critical_branch in self.input_dataset.mc_critical_branches.items():
             flow_values = [
                 sum([mc_market_area_ptdf.da_ptdf.get_value(time)
-                     for mc_market_area_ptdf in mc_critical_branch.market_area_ptdf.values()])
+                     for mc_market_area_ptdf in mc_critical_branch.market_area_ptdf])
                 for time_index, time in enumerate(self.input_dataset.times)]
 
             flow = Timeseries.from_values(
@@ -414,7 +415,7 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
                     mc_critical_branch.da_flow = self.update_timeseries(
                         mc_critical_branch.da_flow, flow)
                 case Product.Intraday:
-                    mc_critical_branch.total_id_flow = self.update_timeseries(
+                    mc_critical_branch.total_id_flow = self.add_timeseries(
                         mc_critical_branch.total_id_flow, flow)
                     mc_critical_branch.id_flow = self.update_forecast_timeseries(
                         mc_critical_branch.id_flow, flow)
@@ -422,19 +423,34 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
                     cfg.logger.info("ATLAS 1.3 does not support exports on critical branches for this market. "
                                 "This should be corrected in future versions")
 
-    def update_timeseries(self, ts_obj, ts_to_add):
+    def add_timeseries(self, ts_obj, ts_to_add):
         """
         Updates a timeseries:
         - if None → assign new_ts
         - else → align timestep and add new values
         """
-        if ts_obj is None:
+        if ts_obj is None or len(ts_obj) < 2:
             return ts_to_add
 
         if ts_obj.timestep != self.input_dataset.parameters.time_step:
             ts_obj.set_frequency(self.input_dataset.parameters.time_step)
 
         ts_obj += ts_to_add
+        return ts_obj
+
+    def update_timeseries(self, ts_obj, new_ts):
+        """
+        Updates a timeseries:
+        - if None → assign new_ts
+        - else → align timestep and add new values
+        """
+        if ts_obj is None or len(ts_obj) < 2:
+            return new_ts
+
+        if ts_obj.timestep != self.input_dataset.parameters.time_step:
+            ts_obj.set_frequency(self.input_dataset.parameters.time_step)
+
+        ts_obj.set_values(new_ts)
         return ts_obj
 
     def update_forecast_timeseries(self, forecast_obj, ts_to_add):
@@ -447,7 +463,8 @@ class MarketClearingOutputDataset(AbstractDataset[MarketClearingParameters]):
                                                   old_ts + ts_to_add)
         return forecast_obj
 
-    def get_custom_business_model_object_modified(self) -> list[type[BusinessModel]]:
+    @staticmethod
+    def get_custom_business_model_object_modified() -> list[type[BusinessModel]]:
         return [MarketArea, MarketBorder, CriticalBranch, Order]
 
     def get_business_model_class_used(self) -> list[type[BusinessModel]]:
