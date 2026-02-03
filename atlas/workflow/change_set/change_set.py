f5bf593dbf5d6598c91dd479549efa1f5a50faac
aboutet <alexis.boutet@artelys.com>
2026-02-03 13:10:33 +0100

    Update change_set

---
+++ atlas/workflow/change_set/change_set.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+from typing import Any, Type
+
+from atlas import BusinessModel
+
+
+class ChangeSet(ABC):
+    @abstractmethod
+    def to_dict(self) -> dict[str, Any]:
+        """Serialize the ChangeSet"""
+        pass
+
+
+# TODO : name must be present !
+class AddObject(ChangeSet):
+    def __init__(self, data: dict[str, Any], model_type: Type[BusinessModel]):
+        self.data = data
+        self.model_type = model_type  # type inferred from obj
+
+    @classmethod
+    def from_obj(cls, obj: BusinessModel):
+        return cls(obj.__dict__, type(obj))
+
+    def to_dict(self) -> dict[str, Any]:
+        return {"kind": "add", "data": self.data}
+
+
+class UpdateObject(ChangeSet):
+    def __init__(self, data: dict[str, Any], model_type: Type[BusinessModel]):
+        self.data = data
+        self.model_type = model_type
+
+    @classmethod
+    def from_obj(cls, obj: BusinessModel):
+        return cls(obj.__dict__, type(obj))
+
+    def to_dict(self) -> dict[str, Any]:
+        return {"kind": "update", "data": self.data}
+
+
+class DeleteObject(ChangeSet):
+    def __init__(self, name: str, model_type: Type[BusinessModel]):
+        self.name = name
+        self.model_type = model_type
+
+    @classmethod
+    def from_obj(cls, obj: BusinessModel):
+        return cls(obj.name, type(obj))
+
+    def to_dict(self) -> dict[str, Any]:
+        return {"kind": "delete", "name": self.name}
--- atlas/workflow/current_input_state.py
+++ atlas/workflow/current_input_state.py
@@ -5,50 +5,9 @@ SPDX-License-Identifier: MPL-2.0
 This file is part of the ATLAS project.
 """
 
-from typing import Iterable
-
-from atlas import BusinessModel
-from atlas.workflow.change_set import ChangeSet
+from atlas.io_utils.atlas_dataset import AtlasDataset
 
 
 class CurrentInputState:
-    def __init__(self, data: dict[str, dict[str, type[BusinessModel]]]):
-        """
-        data example:
-        {
-            "equipment": {
-                "equipment_1": Equipment(...)
-            }
-        }
-        """
+    def __init__(self, data: AtlasDataset):
         self.data = data
-
-    def apply(self, change: ChangeSet) -> None:
-        """
-        Apply one ChangeSet.
-        """
-        change.apply(self.data)
-
-    def apply_all(self, changes: Iterable[ChangeSet]) -> None:
-        """
-        Apply a list of ChangeSet
-        If an error occurs, apply rollbacks
-        """
-        applied: list[ChangeSet] = []
-        next_to_applied = None
-
-        try:
-            for change in changes:
-                next_to_applied = change
-                change.apply(self.data)
-                applied.append(change)
-        except Exception:
-            # rollback applied ChangeSet in reverse order
-            error = f"Error when applying change set : {next_to_applied.__class__}"
-            for change in reversed(applied):
-                try:
-                    change.rollback(self.data)
-                except Exception:
-                    # rollback best-effort
-                    raise ValueError(f"{error}\nCould not rollback change set : {change}")
-            raise ValueError(error)
