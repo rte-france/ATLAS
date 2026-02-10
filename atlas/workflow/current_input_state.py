10340a411ab245947d958ca2507a5ed2edfd3538
aboutet <alexis.boutet@artelys.com>
2026-02-10 12:37:35 +0100

    Add function on CIS to get a filtered dataset

--- atlas/workflow/current_input_state.py
+++ atlas/workflow/current_input_state.py
@@ -5,9 +5,43 @@ SPDX-License-Identifier: MPL-2.0
 This file is part of the ATLAS project.
 """
 
+from collections.abc import Callable, Iterable
+
+from atlas import BusinessModel
+from atlas.enum import BusinessModelName
 from atlas.io_utils.atlas_dataset import AtlasDataset
+from atlas.io_utils.container import Container
 
 
 class CurrentInputState:
     def __init__(self, data: AtlasDataset):
         self.data = data
+
+    def filter_dataset(
+        self,
+        included_types: Iterable[str | BusinessModelName] = (),
+        filters: dict[str | BusinessModelName, Callable[[BusinessModel], bool]] = None,
+    ) -> AtlasDataset:
+        filtered_data: dict[str, list[BusinessModel]] = {}
+
+        for object_type in included_types:
+            object_type_str = object_type.value if isinstance(object_type, BusinessModelName) else object_type
+            if not filters or object_type_str not in filters:
+                try:
+                    container: Container[BusinessModel] = self.data.get_container_by_type(object_type_str)
+                    filtered_data[object_type_str] = list(container)
+                except ValueError:
+                    # skip unknown types
+                    continue
+
+        # Apply filters
+        if filters:
+            for object_type, filter_fn in filters.items():
+                object_type_str = object_type.value if isinstance(object_type, BusinessModelName) else object_type
+                try:
+                    container: Container[BusinessModel] = self.data.get_container_by_type(object_type_str)
+                    filtered_data[object_type_str] = [obj for obj in container if filter_fn(obj)]
+                except ValueError:
+                    continue
+
+        return AtlasDataset.from_dict(filtered_data)
