6ad90e5779c80dd37abfc73bb18d279dd1378d7d
lucarammel <lucaspereira0497@gmail.com>
2026-02-17 16:33:47 +0100

    fix: tests

--- tests/test_unit/test_typing.py
+++ tests/test_unit/test_typing.py
@@ -23,8 +23,17 @@ class DummyConfig:
     MODEL_MAPPING_NAME = {"dummy": type("Dummy", (), {"model_fields": DummyModel.model_fields})}
 
 
+_original_model_mapping_name = None
+
+
 def setup_module(module):
+    global _original_model_mapping_name
+    _original_model_mapping_name = atlas_typing.cfg.MODEL_MAPPING_NAME
     atlas_typing.cfg.MODEL_MAPPING_NAME = DummyConfig.MODEL_MAPPING_NAME
+
+
+def teardown_module(module):
+    atlas_typing.cfg.MODEL_MAPPING_NAME = _original_model_mapping_name
 
 
 def test_get_type_attribute_int():
--- tests/test_unit/test_workflow/test_workflow.py
+++ tests/test_unit/test_workflow/test_workflow.py
@@ -38,34 +38,32 @@ def workflow_parameters(tmp_paths, step_file):
     )
 
 
-def test_build_generic_module_parameters_loads_values(tmp_path, workflow_parameters):
+@patch("atlas.workflow.workflow.ModuleRegistry.get", return_value=DummyModule)
+def test_build_generic_module_parameters_loads_values(mock_get, tmp_path, workflow_parameters):
     yaml_file = tmp_path / "generic.yaml"
     yaml_file.write_text("param1: 42\nparam2: 'test'")
     workflow_parameters.generic_module_parameters_path = yaml_file
 
     wf = Workflow(workflow_parameters)
-    wf.build_generic_module_parameters()
 
     assert wf.generic_module_parameters["param1"] == 42
     assert wf.generic_module_parameters["param2"] == "test"
 
 
-def test_build_generic_module_parameters_no_path(workflow_parameters):
+@patch("atlas.workflow.workflow.ModuleRegistry.get", return_value=DummyModule)
+def test_build_generic_module_parameters_no_path(mock_get, workflow_parameters):
     wf = Workflow(workflow_parameters)
-    wf.build_generic_module_parameters()
     assert wf.generic_module_parameters == {}
 
 
-def test_build_step_unknown_module_raises(workflow_parameters, step_file):
-    wf = Workflow(workflow_parameters)
+def test_build_step_unknown_module_raises(workflow_parameters):
     with pytest.raises(ValueError):
-        wf.build_step(workflow_parameters.steps[0])
+        Workflow(workflow_parameters)
 
 
 @patch("atlas.workflow.workflow.ModuleRegistry.get", return_value=DummyModule)
 def test_build_step_creates_workflowstep(mock_get, workflow_parameters, step_file):
     wf = Workflow(workflow_parameters)
-    wf.build_step(workflow_parameters.steps[0])
 
     assert len(wf.steps) == 1
     step = wf.steps[0]
@@ -78,10 +76,9 @@ def test_build_step_creates_workflowstep(mock_get, workflow_parameters, step_fil
 @patch("atlas.workflow.workflow.ModuleRegistry.get", return_value=DummyModule)
 def test_get_output_dataset_returns_last_step_output(mock_get, workflow_parameters):
     wf = Workflow(workflow_parameters)
-    wf.build_step(workflow_parameters.steps[0])
 
     last_dataset = MagicMock()
-    wf.steps[0]._output_dataset = last_dataset
+    wf.steps[-1]._output_dataset = last_dataset
 
     assert wf.get_output_dataset() == last_dataset
 
@@ -97,8 +94,6 @@ def test_build_steps_merges_generic_and_custom(mock_get, tmp_path, workflow_para
     workflow_parameters.generic_module_parameters_path = generic_file
 
     wf = Workflow(workflow_parameters)
-    wf.build_generic_module_parameters()
-    wf.build_steps()
 
     step = wf.steps[0]
     assert step.parameters["common"] == 1
