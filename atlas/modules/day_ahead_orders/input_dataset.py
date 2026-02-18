025169d9543f288ad0ac0b86425199615d6e5945
aboutet <alexis.boutet@artelys.com>
2026-02-18 16:15:14 +0100

    Update input and output casting

--- atlas/modules/day_ahead_orders/input_dataset.py
+++ atlas/modules/day_ahead_orders/input_dataset.py
@@ -11,39 +11,39 @@ from atlas import (
     AtlasDataset,
     BusinessModel,
     ControlBlock,
-    Hydro,
-    Load,
-    MarketArea,
     MarketBorder,
     Node,
     OtherNonDispatchable,
-    Portfolio,
-    Solar,
-    Storage,
-    Thermal,
-    Wind,
 )
 from atlas.abstract_class.abstract_dataset import AbstractDataset
+from atlas.modules.day_ahead_orders.data_models.hydro import HydroDAO
+from atlas.modules.day_ahead_orders.data_models.load import LoadDAO
+from atlas.modules.day_ahead_orders.data_models.market_area import MarketAreaDAO
+from atlas.modules.day_ahead_orders.data_models.portfolio import PortfolioDAO
+from atlas.modules.day_ahead_orders.data_models.solar import SolarDAO
+from atlas.modules.day_ahead_orders.data_models.storage import StorageDAO
+from atlas.modules.day_ahead_orders.data_models.thermal import ThermalDAO
+from atlas.modules.day_ahead_orders.data_models.wind import WindDAO
 from atlas.modules.day_ahead_orders.parameters import DayAheadOrdersParameters
 
 
 class DayAheadOrdersInputDataset(AbstractDataset[DayAheadOrdersParameters]):
     def __init__(self, raw_data: AtlasDataset, parameters: DayAheadOrdersParameters):
         self.parameters: DayAheadOrdersParameters = parameters
-        self.control_block: list[ControlBlock] = [cast(ControlBlock, obj) for obj in raw_data.control_block]
-        self.market_area: list[MarketArea] = [cast(MarketArea, obj) for obj in raw_data.market_area]
+        self.control_block: list[ControlBlock] = list(raw_data.control_block)
+        self.market_area: list[MarketAreaDAO] = [cast(MarketAreaDAO, obj) for obj in raw_data.market_area]
         self.market_border: list[MarketBorder] = [cast(MarketBorder, obj) for obj in raw_data.market_border]
         self.node: list[Node] = [cast(Node, obj) for obj in raw_data.node]
-        self.portfolio: list[Portfolio] = [cast(Portfolio, obj) for obj in raw_data.portfolio]
-        self.wind: list[Wind] = [cast(Wind, obj) for obj in raw_data.wind]
-        self.storage: list[Storage] = [cast(Storage, obj) for obj in raw_data.storage]
-        self.hydro: list[Hydro] = [cast(Hydro, obj) for obj in raw_data.hydro]
-        self.solar: list[Solar] = [cast(Solar, obj) for obj in raw_data.solar]
-        self.thermal: list[Thermal] = [cast(Thermal, obj) for obj in raw_data.thermal]
+        self.portfolio: list[PortfolioDAO] = [cast(PortfolioDAO, obj) for obj in raw_data.portfolio]
+        self.wind: list[WindDAO] = [cast(WindDAO, obj) for obj in raw_data.wind]
+        self.storage: list[StorageDAO] = [cast(StorageDAO, obj) for obj in raw_data.storage]
+        self.hydro: list[HydroDAO] = [cast(HydroDAO, obj) for obj in raw_data.hydro]
+        self.solar: list[SolarDAO] = [cast(SolarDAO, obj) for obj in raw_data.solar]
+        self.thermal: list[ThermalDAO] = [cast(ThermalDAO, obj) for obj in raw_data.thermal]
         self.other_non_dispatchable: list[OtherNonDispatchable] = [
             cast(OtherNonDispatchable, obj) for obj in raw_data.other_non_dispatchable
         ]
-        self.load: list[Load] = [cast(Load, obj) for obj in raw_data.load]
+        self.load: list[LoadDAO] = [cast(LoadDAO, obj) for obj in raw_data.load]
 
     def get_business_model_class_used(self) -> list[type[BusinessModel]]:
         return []
--- atlas/modules/day_ahead_orders/output_dataset.py
+++ atlas/modules/day_ahead_orders/output_dataset.py
@@ -6,7 +6,6 @@ This file is part of the ATLAS project.
 """
 
 import copy
-from typing import cast
 
 from atlas import (
     ControlBlock,
@@ -26,8 +25,10 @@ from atlas.abstract_class.abstract_dataset import AbstractDataset
 from atlas.models.business_model import BusinessModel
 from atlas.modules.day_ahead_orders.data_models.hydro import HydroDAO
 from atlas.modules.day_ahead_orders.data_models.load import LoadDAO
+from atlas.modules.day_ahead_orders.data_models.market_area import MarketAreaDAO
 from atlas.modules.day_ahead_orders.data_models.order import OrderDAO
 from atlas.modules.day_ahead_orders.data_models.order_coupling import OrderCouplingDAO
+from atlas.modules.day_ahead_orders.data_models.portfolio import PortfolioDAO
 from atlas.modules.day_ahead_orders.data_models.solar import SolarDAO
 from atlas.modules.day_ahead_orders.data_models.storage import StorageDAO
 from atlas.modules.day_ahead_orders.data_models.thermal import ThermalDAO
@@ -40,24 +41,18 @@ class DayAheadOrdersOutput(AbstractDataset[DayAheadOrdersParameters]):
     def __init__(self, input_dataset: DayAheadOrdersInputDataset):
         self.parameters: DayAheadOrdersParameters = copy.deepcopy(input_dataset.parameters)
         self.control_block: list[ControlBlock] = copy.deepcopy(input_dataset.control_block)
-        self.market_area: list[MarketArea] = copy.deepcopy(input_dataset.market_area)
+        self.market_area: list[MarketAreaDAO] = copy.deepcopy(input_dataset.market_area)
         self.market_border: list[MarketBorder] = copy.deepcopy(input_dataset.market_border)
         self.node: list[Node] = copy.deepcopy(input_dataset.node)
-        self.portfolio: list[Portfolio] = copy.deepcopy(input_dataset.portfolio)
+        self.portfolio: list[PortfolioDAO] = copy.deepcopy(input_dataset.portfolio)
         self.other_non_dispatchable: list[OtherNonDispatchable] = copy.deepcopy(input_dataset.other_non_dispatchable)
 
-        input_load: list[Load] = copy.deepcopy(input_dataset.load)
-        self.load: list[LoadDAO] = [cast(LoadDAO, obj) for obj in input_load]
-        input_storage: list[Storage] = copy.deepcopy(input_dataset.storage)
-        self.storage: list[StorageDAO] = [cast(StorageDAO, obj) for obj in input_storage]
-        input_hydro: list[Hydro] = copy.deepcopy(input_dataset.hydro)
-        self.hydro: list[HydroDAO] = [cast(HydroDAO, obj) for obj in input_hydro]
-        input_solar: list[Solar] = copy.deepcopy(input_dataset.solar)
-        self.solar: list[SolarDAO] = [cast(SolarDAO, obj) for obj in input_solar]
-        input_thermal: list[Thermal] = copy.deepcopy(input_dataset.thermal)
-        self.thermal: list[ThermalDAO] = [cast(ThermalDAO, obj) for obj in input_thermal]
-        input_wind: list[Wind] = copy.deepcopy(input_dataset.wind)
-        self.wind: list[WindDAO] = [cast(WindDAO, obj) for obj in input_wind]
+        self.load: list[LoadDAO] = copy.deepcopy(input_dataset.load)
+        self.storage: list[StorageDAO] = copy.deepcopy(input_dataset.storage)
+        self.hydro: list[HydroDAO] = copy.deepcopy(input_dataset.hydro)
+        self.solar: list[SolarDAO] = copy.deepcopy(input_dataset.solar)
+        self.thermal: list[ThermalDAO] = copy.deepcopy(input_dataset.thermal)
+        self.wind: list[WindDAO] = copy.deepcopy(input_dataset.wind)
 
         self.order: list[OrderDAO] = []
         self.order_coupling: list[OrderCouplingDAO] = []
