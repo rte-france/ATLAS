eb26d3ca1f052847b1c45ada5ecaf5202a5c5f7b
aboutet <alexis.boutet@artelys.com>
2026-02-23 15:43:34 +0100

    Create change set of DAO

--- atlas/modules/day_ahead_orders/output_dataset.py
+++ atlas/modules/day_ahead_orders/output_dataset.py
@@ -22,6 +22,7 @@ from atlas import (
     Wind,
 )
 from atlas.abstract_class.abstract_dataset import AbstractModuleOutput
+from atlas.enums import ThermalStrategy
 from atlas.models.business_model import BusinessModel
 from atlas.modules.day_ahead_orders.data_models.hydro import HydroDAO
 from atlas.modules.day_ahead_orders.data_models.load import LoadDAO
@@ -35,6 +36,7 @@ from atlas.modules.day_ahead_orders.data_models.thermal import ThermalDAO
 from atlas.modules.day_ahead_orders.data_models.wind import WindDAO
 from atlas.modules.day_ahead_orders.input_dataset import DayAheadOrdersInputDataset
 from atlas.modules.day_ahead_orders.parameters import DayAheadOrdersParameters
+from atlas.workflow.change_set import AddObject, UpdateObject
 
 
 class DayAheadOrdersOutput(AbstractModuleOutput[DayAheadOrdersParameters]):
@@ -74,4 +76,45 @@ class DayAheadOrdersOutput(AbstractModuleOutput[DayAheadOrdersParameters]):
         ]
 
     def build_change_sets(self):
-        return super().build_change_sets()
+        for order in self.order:
+            self.change_sets.append(AddObject.from_object(order))
+        for order_coupling in self.order_coupling:
+            self.change_sets.append(AddObject.from_object(order_coupling))
+
+        for load in self.load:
+            load_dict = {"name": load.name, "da_buy_submitted_volume": load.da_buy_submitted_volume}
+            self.change_sets.append(UpdateObject(load_dict, Load))
+
+        for hydro in self.hydro:
+            hydro_dict = {"name": hydro.name, "da_sell_submitted_volume": hydro.da_sell_submitted_volume}
+            self.change_sets.append(UpdateObject(hydro_dict, Hydro))
+
+        for solar in self.solar:
+            solar_dict = {"name": solar.name, "da_sell_submitted_volume": solar.da_sell_submitted_volume}
+            self.change_sets.append(UpdateObject(solar_dict, Solar))
+
+        for wind in self.wind:
+            wind_dict = {"name": wind.name, "da_sell_submitted_volume": wind.da_sell_submitted_volume}
+            self.change_sets.append(UpdateObject(wind_dict, Wind))
+
+        for other_non_dispatchable in self.other_non_dispatchable:
+            other_non_dispatchable_dict = {
+                "name": other_non_dispatchable.name,
+                "da_sell_submitted_volume": other_non_dispatchable.da_sell_submitted_volume,
+            }
+            self.change_sets.append(UpdateObject(other_non_dispatchable_dict, OtherNonDispatchable))
+
+        for thermal in self.thermal:
+            thermal_dict = {"name": thermal.name, "da_sell_submitted_volume": thermal.da_sell_submitted_volume}
+            if thermal.strategy == ThermalStrategy.INTERMEDIATE:
+                thermal_dict["state_sequence"] = thermal.state_sequence
+            self.change_sets.append(UpdateObject(thermal_dict, Thermal))
+
+        for storage in self.storage:
+            storage_dict = {
+                "name": storage.name,
+                "da_sell_submitted_volume": storage.da_sell_submitted_volume,
+                "da_buy_submitted_volume": storage.da_buy_submitted_volume,
+                "variable_cost": storage.variable_cost,
+            }
+            self.change_sets.append(UpdateObject(storage_dict, Thermal))
