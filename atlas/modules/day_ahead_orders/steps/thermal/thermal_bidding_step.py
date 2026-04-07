0aa780a972217e26e3072cc3f38c800c0984a583
lucarammel <lucaspereira0497@gmail.com>
2026-04-07 13:55:51 +0200

    feat: sort imports

--- atlas/modules/day_ahead_orders/steps/thermal/thermal_bidding_step.py
+++ atlas/modules/day_ahead_orders/steps/thermal/thermal_bidding_step.py
@@ -15,9 +15,9 @@ from atlas import Thermal, Timeseries
 from atlas.enums import CouplingType, Product, ThermalStrategy
 from atlas.models.market.order import Order
 from atlas.modules.day_ahead_orders.models.order import OrderDAO
-from atlas.modules.day_ahead_orders.steps.thermal.thermal_worker import optimize_single_thermal_unit
 from atlas.modules.day_ahead_orders.output_dataset import DayAheadOrdersOutput
 from atlas.modules.day_ahead_orders.parameters import DayAheadOrdersParameters
+from atlas.modules.day_ahead_orders.steps.thermal.thermal_worker import optimize_single_thermal_unit
 
 
 class Coupling:
