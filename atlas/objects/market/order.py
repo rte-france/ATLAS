2d9f72414c60aec409a612a275e1c28d567e432f
lucarammel <lucaspereira0497@gmail.com>
2026-04-10 19:38:48 +0200

    feat: quick fix on none types for order

--- atlas/objects/market/order.py
+++ atlas/objects/market/order.py
@@ -51,9 +51,9 @@ class Order(BusinessModel):
     :type q_min: float
     """
 
-    equipment: Equipment
+    equipment: Equipment | None = None
     market_area: MarketArea
-    portfolio: Portfolio
+    portfolio: Portfolio | None = None
     accepted_power: float | None = None
     execution_date: DateTime | None = None
     start_date: DateTime | None = None
