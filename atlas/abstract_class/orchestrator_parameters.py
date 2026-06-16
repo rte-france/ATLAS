1cec3dea7aaf64514b71c5965eadb26d5aa83681
Alexandre Le Jean <alexandre.le-jean@artelys.com>
2026-06-16 09:21:39 +0200

    removed unused parameter

--- atlas/abstract_class/orchestrator_parameters.py
+++ atlas/abstract_class/orchestrator_parameters.py
@@ -34,7 +34,6 @@ class AbstractOrchestratorParameters(Parameters):
 
     name: str | None = None
     dataset_path: Path
-    parameters_path: Path | None = None  # FIXME unused?
     path_from_workflow: bool = Field(
         default=True,
         validation_alias=AliasChoices("path_from_workflow", "path_from_orchestrator", "path_from_action_plan"),
