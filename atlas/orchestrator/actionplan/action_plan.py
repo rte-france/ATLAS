2d98f775f705c57bb813bd3e186743a060ea2a17
Alexandre Le Jean <alexandre.le-jean@artelys.com>
2026-08-27 08:53:25 +0200

    refactoring: simplify action_plan.py code
    Removed priority queue as a property, it is now created and exploited in the functions job();
    
    The removed property is replaced by a simple list, easier to interact with other functions.

--- atlas/orchestrator/actionplan/action_plan.py
+++ atlas/orchestrator/actionplan/action_plan.py
@@ -42,14 +42,15 @@ class ActionPlan(AbstractOrchestrator[ActionPlanParameters, ActionPlanJob]):
         :type parameters: WorkflowParameters
         """
         super().__init__(parameters)
-        self._priority_queue: list[tuple[TaskIterationPriority, int, TaskJobsGenerator]] = []
-        self._build_priority_queue()
+        self._task_job_generators: list[TaskJobsGenerator] = []
+        for task in self.parameters.tasks:
+            self.add_task(task)
 
     def _has_concurrent_task_with(self, task: Task) -> bool:
-        return any(Task.are_concurrent(itr.task, task) for _, _, itr in self._priority_queue)
+        return any(Task.are_concurrent(itr.task, task) for itr in self._task_job_generators)
 
-    def add_task(self, task: TaskModule | TaskWorkflow) -> int:
-        """Add a task to the action plan and return the number of jobs added
+    def add_task(self, task: TaskModule | TaskWorkflow):
+        """Add a task to the action plan
         :param task: task to add
         :type task: Task
         """
@@ -64,16 +65,14 @@ class ActionPlan(AbstractOrchestrator[ActionPlanParameters, ActionPlanJob]):
         if task_adder is None:
             raise ValueError(f"Unknown type {type(task)} when adding {task} to Action Plan {self}")
 
-        return task_adder(task, root_output_dir, 0)
+        task_adder(task, root_output_dir)
 
-    def _add_task_module(self, task: TaskModule, root_output_dir: Path, next_iteration: int) -> int:
-        """Add a task, that run a module, and the iteration progress on it to the action plan and return the number of job add
+    def _add_task_module(self, task: TaskModule, root_output_dir: Path):
+        """Add a task, that run a module, and the iteration progress on it to the action plan
         :param task: task that run a module
         :type task: TaskModule
         :param root_output_dir: path to the root output directory used for the task
         :type root_output_dir: Path
-        :param next_iteration: next iteration for which the task will be used
-        :type next_iteration: int
         """
         if task.module is None:
             raise ValueError(f"Fail to add Task {task.name}, a TaskModule with module value None.")
@@ -85,16 +84,14 @@ class ActionPlan(AbstractOrchestrator[ActionPlanParameters, ActionPlanJob]):
             .from_file(self.parameters.resolve_path(task.parameters_path), self.parameters.context)
         )
         module_iterator = ModuleTaskJobsGenerator(task, module_parameters, root_output_dir)
-        return self._push_iterator(module_iterator, next_iteration)
+        self._task_job_generators.append(module_iterator)
 
-    def _add_task_workflow(self, task: TaskWorkflow, root_output_dir: Path, next_iteration: int) -> int:
-        """Add a task, that run a workflow, to the action plan and return the number of job add
+    def _add_task_workflow(self, task: TaskWorkflow, root_output_dir: Path):
+        """Add a task, that run a workflow, to the action plan
         :param task: task that run a workflow
         :type task: TaskWorkflow
         :param root_output_dir: path to the root output directory used for the task
         :type root_output_dir: Path
-        :param next_iteration: next iteration for which the task will be used
-        :type next_iteration: int
         """
         if isinstance(task.workflow, Path):
             workflow_parameters = WorkflowParameters.from_file(self.parameters.resolve_path(task.workflow))
@@ -102,29 +99,7 @@ class ActionPlan(AbstractOrchestrator[ActionPlanParameters, ActionPlanJob]):
             workflow_parameters = task.workflow.parameters
         workflow_parameters.context.use(self.parameters.context)
         workflow_iterator = WorkflowTaskJobsGenerator(task, workflow_parameters, root_output_dir)
-        return self._push_iterator(workflow_iterator, next_iteration)
-
-    def _push_iterator(self, iterator: TaskJobsGenerator, next_iteration: int) -> int:
-        """Add an iterator to the priority queue and return the number of jobs added
-        :param iterator: iterator to add
-        :type iterator: TaskGenerator
-        :param next_iteration: next iteration for which the task will be used
-        :type next_iteration: int
-        """
-        if self._has_concurrent_task_with(iterator.task):
-            raise ValueError("Try to add a concurrent task to the Action plan")
-        heapq.heappush(self._priority_queue, (iterator.priority(next_iteration), next_iteration, iterator))
-        return len(iterator)
-
-    def _pop_iterator(self):
-        """Remove and return the next iterator in the priority queue"""
-        _, iteration, task_generator = heapq.heappop(self._priority_queue)
-        return iteration, task_generator
-
-    def _build_priority_queue(self):
-        self._priority_queue.clear()
-        for task in self.parameters.tasks:
-            self.add_task(task)
+        self._task_job_generators.append(workflow_iterator)
 
     @property
     def jobs(self) -> Iterator[ActionPlanJob]:
@@ -133,19 +108,22 @@ class ActionPlan(AbstractOrchestrator[ActionPlanParameters, ActionPlanJob]):
 
         :return: The list of ActionPlanJob instances.
         """
-        cc = copy.deepcopy(self._priority_queue)
-        while len(self._priority_queue) > 0:
-            next_iteration, job_generator = self._pop_iterator()
-            jobs = job_generator.build_jobs(next_iteration)
-            if jobs is not None:
-                self._push_iterator(job_generator, next_iteration + 1)
-                for job in jobs:
-                    yield cast(ActionPlanJob, job)
-        self._priority_queue = cc
+        priority_queue: list[tuple[TaskIterationPriority, int, TaskJobsGenerator]] = []
+        for task_generator in self._task_job_generators:
+            heapq.heappush(priority_queue, (task_generator.priority(1), 1, task_generator))
+
+        while len(priority_queue) > 0:
+            _, current_iteration, job_generator = heapq.heappop(priority_queue)
+            jobs = job_generator.build_jobs(current_iteration)
+            for job in jobs:
+                yield cast(ActionPlanJob, job)
+            next_iteration = current_iteration + 1
+            if job_generator.is_valid_iteration(next_iteration):
+                heapq.heappush(priority_queue, (job_generator.priority(next_iteration), next_iteration, job_generator))
 
     @property
     def jobs_count(self) -> int:
-        return sum(len(itr) for _, _, itr in self._priority_queue)
+        return sum(len(itr) for itr in self._task_job_generators)
 
     def __repr__(self) -> str:
         """Return a human-readable string representation of the workflow."""
