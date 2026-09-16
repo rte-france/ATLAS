61f6f6c9905f983911242a002b2bc310e78b417b
Alexandre Le Jean <alexandre.le-jean@artelys.com>
2026-09-16 10:53:54 +0200

    fix: now properly make a deep copy of parameters in Step

--- atlas/orchestrator/workflow/workflow.py
+++ atlas/orchestrator/workflow/workflow.py
@@ -85,6 +85,8 @@ class Workflow(AbstractOrchestrator[WorkflowParameters, WorkflowJob]):
             )
         elif isinstance(parameters, dict):
             parameters = parameters_class.from_dict(parameters, self.parameters.context)
+        elif isinstance(parameters, AbstractModuleParameters):
+            parameters = parameters.model_copy()
         return parameters
 
     def __repr__(self) -> str:
